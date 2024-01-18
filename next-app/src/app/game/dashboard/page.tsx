b4e783eede70dd9b646db30eba72907e53ad665d
KLociJS <kisznerlorant21@gmail.com>
2024-01-18 19:53:38 +0100

    feat: Implement dashboard tables partially

--- next-app/src/app/game/dashboard/page.tsx
+++ next-app/src/app/game/dashboard/page.tsx
@@ -4,6 +4,8 @@ import { SOCKET_EVENTS } from "@/app/_constants/constants";
 import useConnectSocket from "@/app/_hooks/useConnectSocket";
 import { useEffect, useState } from "react";
 
+import Styles from "./dashboard.module.css";
+
 type Player = {
   id: string;
   name: string;
@@ -13,12 +15,16 @@ type Player = {
 type Game = {
   id: string;
   name: string;
+  limit: number;
+  stakes: string;
   maxPlayers: number;
   players: Player[];
 };
 
 const Dashboard = () => {
   const [games, setGames] = useState<Game[]>([]);
+  const [selectedGame, setSelectedGame] = useState<Game | null>(null);
+
   const { socket } = useConnectSocket();
 
   useEffect(() => {
@@ -36,16 +42,76 @@ const Dashboard = () => {
     });
   };
 
+  const selectGame = (id: string) => {
+    const selectedGame = games.find((game) => game.id === id);
+    if (selectedGame) {
+      setSelectedGame(selectedGame);
+    }
+  };
+
   return (
     <>
-      <main>
-        <h1>Dashboard</h1>
-        <ul>
-          {games.map((game) => (
-            <li key={game.id}>{game.name}</li>
-          ))}
-        </ul>
-        <button onClick={createGame}> Create game </button>
+      <main className={Styles.main_container}>
+        <div className={Styles.search_container}>
+          <div className={Styles.input_container}>
+            <label className={Styles.label} htmlFor='limit'>
+              Limit
+            </label>
+            <select className={Styles.select} name='limit'>
+              <option value='name' className={Styles.option}>
+                Name
+              </option>
+              <option value='id' className={Styles.option}>
+                ID
+              </option>
+            </select>
+          </div>
+
+          <div className={Styles.input_container}>
+            <label className={Styles.label} htmlFor='stakes'>
+              Stakes
+            </label>
+            <select className={Styles.select} name='stakes'>
+              <option value='name'>Name</option>
+              <option value='id'>ID</option>
+            </select>
+          </div>
+
+          <div className={Styles.input_container}>
+            <label className={Styles.label} htmlFor='max players'>
+              Max players
+            </label>
+            <select className={Styles.select} name='max players'>
+              <option value='name'>Name</option>
+              <option value='id'>ID</option>
+            </select>
+          </div>
+        </div>
+        <div className={Styles.tables_container}>
+          <table className={Styles.rooms_table}>
+            <thead>
+              <tr>
+                <th>Table Name</th>
+                <th>Limit</th>
+                <th>Stakes</th>
+                <th>Players</th>
+              </tr>
+            </thead>
+            <tbody>
+              {games.map((game) => (
+                <tr key={game.id} onClick={() => selectGame(game.id)}>
+                  <td>{game.name}</td>
+                  <td>{game.limit}</td>
+                  <td>{game.stakes}</td>
+                  <td>{`${game.players.length} / ${game.maxPlayers}`}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <div className={Styles.players_table_container}>
+            {selectedGame && <h1>{selectedGame.name}</h1>}
+          </div>
+        </div>
       </main>
     </>
   );
--- websocket/src/actions/socketAction.js
+++ websocket/src/actions/socketAction.js
@@ -1,6 +1,79 @@
 const { Player, Room } = require("../classes/gameClasses");
 
-const games = [];
+const games = [
+  {
+    id: "1",
+    name: "Game 1",
+    limit: "Fix",
+    stakes: "0.1$ / 0.2$",
+    maxPlayers: 9,
+    players: [],
+  },
+  {
+    id: "2",
+    name: "Game 2",
+    limit: "Fix",
+    stakes: "0.1$ / 0.2$",
+    maxPlayers: 9,
+    players: [],
+  },
+  {
+    id: "3",
+    name: "Game 3",
+    limit: "Fix",
+    stakes: "0.1$ / 0.2$",
+    maxPlayers: 9,
+    players: [],
+  },
+  {
+    id: "4",
+    name: "Game 4",
+    limit: "Fix",
+    stakes: "0.1$ / 0.2$",
+    maxPlayers: 9,
+    players: [],
+  },
+  {
+    id: "5",
+    name: "Game 5",
+    limit: "Fix",
+    stakes: "0.1$ / 0.2$",
+    maxPlayers: 9,
+    players: [],
+  },
+  {
+    id: "6",
+    name: "Game 6",
+    limit: "Fix",
+    stakes: "0.1$ / 0.2$",
+    maxPlayers: 9,
+    players: [],
+  },
+  {
+    id: "7",
+    name: "Game 7",
+    limit: "Fix",
+    stakes: "0.1$ / 0.2$",
+    maxPlayers: 9,
+    players: [],
+  },
+  {
+    id: "8",
+    name: "Game 8",
+    limit: "Fix",
+    stakes: "0.1$ / 0.2$",
+    maxPlayers: 9,
+    players: [],
+  },
+  {
+    id: "9",
+    name: "Game 9",
+    limit: "Fix",
+    stakes: "0.1$ / 0.2$",
+    maxPlayers: 9,
+    players: [],
+  },
+];
 
 const createGame = (roomName) => {
   const room = new Room(roomName);
