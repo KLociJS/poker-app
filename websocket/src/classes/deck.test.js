4d41b7ef6868d235b036e666650bd14b51e76183
KLociJS <kisznerlorant21@gmail.com>
2024-03-21 15:29:14 +0100

    test: Write unit tests for deck

---
+++ websocket/src/classes/deck.test.js
@@ -0,0 +1,36 @@
+const { DECK } = require("../constants/cards");
+const Deck = require("./deck");
+
+describe("Deck", () => {
+  let deck;
+
+  beforeEach(() => {
+    deck = new Deck();
+  });
+
+  it("should initialize with an empty array of cards", () => {
+    expect(deck.cards).toEqual([]);
+  });
+
+  it("should draw a card from the deck", () => {
+    deck.cards = [...DECK];
+    const drawnCard = deck.drawCard();
+    expect(drawnCard).toStrictEqual({ rank: "A", suit: "club" });
+    expect(deck.cards).toEqual([...DECK].slice(0, -1));
+  });
+
+  it("should burn a card from the deck", () => {
+    deck.cards = [...DECK];
+    deck.burnCard();
+    expect(deck.cards).toEqual([...DECK].slice(0, -1));
+  });
+
+  it("should shuffle the deck", () => {
+    const originalCards = [...DECK];
+    deck.shuffle();
+    expect(deck.cards.length).toBe(originalCards.length);
+    originalCards.forEach((card) => {
+      expect(deck.cards).toContain(card);
+    });
+  });
+});
